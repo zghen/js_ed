d4b118eb2c25bd7779d4b2890b9967d3b7c168fa
Paul <pay@qapint.com>
2016-02-01 15:44:26 +0200

    index.js refactoring, div correct generation

--- index.js
+++ index.js
@@ -1,9 +1,92 @@
-var Item = function(name, price) {
+var counter = 0;
+
+var createCat = document.getElementById('createCat');
+
+createCat.addEventListener('click', function(){
+	var name = document.getElementById('catName').value;
+	var price = document.getElementById('catPrice').value;
+	var src = document.getElementById('catSrc').value;
+	generateCat(name, price, src);
+});
+
+var Cat = function(name, price, src) {
 	this.name = name;
 	this.price = price;
-}
+	this.bought = false;
+	this.src = src;
+	this.id = 'Cat' + ++counter;
+ };
 
-var Cashier = {
+var Basket = {
+		catArray : [],
+		addCat : function(cat) {			
+			Basket.catArray.push(cat);
+		},
+		removeCat : function(cat) {
+
+			Basket.catArray[0];			
+		}
+};
+
+
+function generateCat(name, price, src){
+
+	cat = new Cat(name, price,src);
+	Basket.addCat(cat);	
+	generate('div');
+	generate('img', '' , src);
+	generate('br');
+	generate('button', 'Add');
+	generate('button', 'Remove');
+	generate('p', price);
+};	
+
+/*removeCat = function(cat) {
+	removeNodeElement('img');
+}
+*/
+function generate(nodeElement){
+	var catNode = document.createElement(nodeElement),
+		parent = document.body;
+		
+		parent.appendChild(catNode);
+		console.log(catNode);
+};
+
+function divAppend(childElement, html, src){
+	var childNode = document.createElement(childElement),
+		parent = document.body;
+
+		if(html){
+			catNode.innerHTML = html;
+		};
+		if(src){
+			catNode.src = src;
+		};
+		generate
+		parent.appendChild(childElement);
+
+
+};
+
+/*removeNodeElement = function(nodeElement){
+	var catNode = document.getElementById('cat');
+	parent = document.body;
+	parent.removeChild(nodeElement);
+}*/
+
+
+
+
+/*buy[0].addEventListener('click', function(){
+	Cashier.getEl();
+	console.log(Cashier.total);
+	lol.innerHTML = 'Total Price = ' + Cashier.total;
+	bought.innerHTML = 'You have bought: ' + BoughtCat;
+});*/
+
+
+/*var Cashier = {
 	total : 0,
 	getEl : function(){
 		Cashier.total = 0;
@@ -20,25 +103,5 @@ var Cashier = {
 		};
 		return Cashier.total;
 	}
-}
-
-var BoughtCat = [];
+}*/
 //input takes this.name and this.price, button creates new Item and push this.item to Cats[]
-var itemCat1 = new Item("Tiger", 10);
-var itemCat2 = new Item("Mishka", 20);
-var itemCat3 = new Item("Sam", 30);
-var itemCat4 = new Item("Sleepy", 40);
-var itemCat5 = new Item("Ryzhyk", 50);
-var Cats = [itemCat1, itemCat2, itemCat3, itemCat4, itemCat5];
-
-var buy = document.getElementsByTagName('button');
-var checkbox = document.getElementsByTagName('input');
-var result = document.getElementById('lol');
-var bought = document.getElementById('cats');
-
-buy[0].addEventListener('click', function(){
-	Cashier.getEl();
-	console.log(Cashier.total);
-	lol.innerHTML = 'Total Price = ' + Cashier.total;
-	bought.innerHTML = 'You have bought: ' + BoughtCat;
-});
