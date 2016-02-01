b27015d581b46d47c3dfa1f8a21f2e61b0382a82
Paul <pay@qapint.com>
2016-02-01 15:52:29 +0200

    index.js fixed

--- index.js
+++ index.js
@@ -30,44 +30,63 @@ var Basket = {
 
 
 function generateCat(name, price, src){
+	var cat = new Cat(name, price, src);
+	Basket.addCat(cat);
+	var div = generateDiv(cat);
+	document.body.appendChild(div);
+	div.button.addEventListener	
+}
 
-	cat = new Cat(name, price,src);
-	Basket.addCat(cat);	
-	generate('div');
-	generate('img', '' , src);
-	generate('br');
-	generate('button', 'Add');
-	generate('button', 'Remove');
-	generate('p', price);
-};	
+function generate(element, html, src) {
+	var generated = document.createElement(element);
+
+		if(html){
+			generated.innerHTML = html;
+		}
+
+		if(src){
+			generated.src = src;
+		}
+
+	return generated;
+}
+
+function generateDiv(cat) {
+	var div = generate('div');
+
+	var img = generate('img', '' , cat.src);
+	var button = generate('button', 'Add');
+	var button2 = generate('button', 'Remove');
+	var p = generate('p', cat.price);
+
+	[img, button, button2, p].forEach(function(element){
+		div.appendChild(element);
+	});
+
+	return div;
+}	
+
 
 /*removeCat = function(cat) {
 	removeNodeElement('img');
 }
 */
-function generate(nodeElement){
-	var catNode = document.createElement(nodeElement),
-		parent = document.body;
-		
-		parent.appendChild(catNode);
-		console.log(catNode);
-};
 
-function divAppend(childElement, html, src){
+/*function generate(childElement, parentElement, html, src, className){
 	var childNode = document.createElement(childElement),
-		parent = document.body;
+		parent = parentElement;
 
 		if(html){
-			catNode.innerHTML = html;
+			childNode.innerHTML = html;
 		};
 		if(src){
-			catNode.src = src;
+			childNode.src = src;
 		};
-		generate
-		parent.appendChild(childElement);
-
-
-};
+		if(className){
+			childNode.className = className;
+		}
+		parent.appendChild(childNode);
+};*/
 
 /*removeNodeElement = function(nodeElement){
 	var catNode = document.getElementById('cat');
@@ -103,5 +122,28 @@ function divAppend(childElement, html, src){
 		};
 		return Cashier.total;
 	}
-}*/
+<<<<<<< 6c034b8832084c1a909d66b9e4a8b00171811469
+}
+
+var BoughtCat = [];
 //input takes this.name and this.price, button creates new Item and push this.item to Cats[]
+var itemCat1 = new Item("Tiger", 10);
+var itemCat2 = new Item("Mishka", 20);
+var itemCat3 = new Item("Sam", 30);
+var itemCat4 = new Item("Sleepy", 40);
+var itemCat5 = new Item("Ryzhyk", 50);
+var Cats = [itemCat1, itemCat2, itemCat3, itemCat4, itemCat5];
+
+var buy = document.getElementsByTagName('button');
+var checkbox = document.getElementsByTagName('input');
+var result = document.getElementById('lol');
+var bought = document.getElementById('cats');
+
+buy[0].addEventListener('click', function(){
+	Cashier.getEl();
+	console.log(Cashier.total);
+	lol.innerHTML = 'Total Price = ' + Cashier.total;
+	bought.innerHTML = 'You have bought: ' + BoughtCat;
+});
+=======
+}*/
